95cecff869964c2d9b8f273b4892fd8829246bfe
Littlebits <100325493+LittlebitsNFT@users.noreply.github.com>
2022-02-27 21:37:44 -0300

    Fix comment with old attribute order

--- contracts/LbCharacter.sol
+++ contracts/LbCharacter.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.0;
  *
  * @title LbCharacter in-development contract 
  * @author gifMaker - contact@littlebits.club
- * @notice v0.8 / 2022
+ * @notice v0.81 / 2022
  * @dev LittlebitsNFT token on-chain metadata -- each token is a Character
  *
  * --== NOT FOR RELEASE ==--
@@ -14,7 +14,7 @@ pragma solidity ^0.8.0;
 contract LbCharacter {
     // AttributeId AttributeKey;
     // 0 gender; 1 rarity; 2 costume; 3 hat; 4 hair; 5 glasses; 6 eyes; 7 nose;
-    // 8 bowtie; 9 beard; 10 shoes; 11 jacket; 12 torso; 13 legs; 14 skin;
+    // 8 beard; 9 bowtie; 10 jacket; 11 torso; 12 legs; 13 shoes; 14 skin;
     struct Character {
         uint8[15] attributes;
         string imageId;
